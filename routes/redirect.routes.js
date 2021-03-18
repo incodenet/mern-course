c80b3662c05fc3e1f5cf03251a2416cc09ac6d38
erik <erikforntdev@gmail.com>
2021-03-18 13:44:24 +0400

    initial commit

---
+++ routes/redirect.routes.js
@@ -0,0 +1,23 @@
+const {Router} = require('express');
+const Link = require('../models/Link');
+const router = Router();
+
+router.get('/:code', async (req, res) => {
+    try {
+        const link = await Link.findOne({code: req.params.code});
+
+        if(link) {
+            link.clicks++;
+
+            await link.save();
+
+            return res.redirect(link.form);
+        }
+
+        res.status(404).json("Link is not defind");
+    } catch (e) {
+        res.status(500).json({ message: 'Error: try again.' })
+    }
+});
+
+module.exports = router;
